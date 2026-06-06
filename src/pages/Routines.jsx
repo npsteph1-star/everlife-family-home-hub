badf2e661a9b94f9075b3f36172ee2d75cf9d0ef
npsteph1-star <npsteph1@gmail.com>
2026-06-06 12:58:16 -0700

    Add Routines page

---
+++ src/pages/Routines.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Routines() {
+  return (
+    <div className="card">
+      <h2>Routines</h2>
+      <p>Manage daily and custom routines.</p>
+    </div>
+  );
+}
