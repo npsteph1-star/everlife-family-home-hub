bbdd598a567988c5f50490bbe93082cb066a0825
npsteph1-star <npsteph1@gmail.com>
2026-06-06 13:01:41 -0700

    Create Meals.jsx

---
+++ src/pages/Meals.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Meals() {
+  return (
+    <div className="card">
+      <h2>Meals & Pantry</h2>
+      <p>Plan meals and manage your grocery list and pantry.</p>
+    </div>
+  );
+}
